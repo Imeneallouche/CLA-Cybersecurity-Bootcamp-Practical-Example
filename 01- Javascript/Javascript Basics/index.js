f165a0103338a5c7dc82689ab78e1edde407e814
Imeneallouche <li_allouche@esi.dz>
2024-05-10 16:14:04 +0100

    Javascript practical examples have been added

---
+++ 01- Javascript/Javascript Basics/index.js
@@ -0,0 +1,15 @@
+// VARIABLES : CONST, LET, VAR
+const name = "Imene";
+
+function income() {
+  let balance = 1000;
+  var country = "Germany";
+}
+
+income();
+country = "Algeria";
+console.log("your country:", country);
+
+balance += 1;
+// balance = balance +1 ;
+console.log("your balance: ", balance);
